6df35703867f9e5319e83b7eb5368fc38d3bb0fc
AANtoso <adjoaan@gmail.com>
2020-06-12 11:25:02 -0400

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 
 class Band extends Component {
+  handleClick = () => {
+    console.log('Click');
+    this.props.deleteBand(this.props.band.id);
+  };
 
   render() {
-    return(
+    return (
       <div>
-        Band Component
+        <li>
+          {this.props.band.name}
+          {console.log(this.props)}
+          <button onClick={this.handleClick}>DELETE</button>
+        </li>
       </div>
     );
   }
-};
+}
 
 export default Band;
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,12 +1,24 @@
-export default function manageBand(state = {
-  bands: []
-}, action) {
+import uuid from 'uuid';
+
+export default function manageBand(
+  state = {
+    bands: [],
+  },
+  action,
+) {
   switch (action.type) {
     case 'ADD_BAND':
+      const band = {
+        id: uuid(),
+        name: action.name,
+      };
+      return { ...state, bands: [...state.bands, band] };
 
-      return { ...state, bands: [...state.bands, action.name] }
+    case 'DELETE_BAND':
+      console.log('DELETE_BAND', action);
+      return { bands: state.bands.filter((b) => b.id !== action.id) };
 
     default:
       return state;
   }
-};
+}
