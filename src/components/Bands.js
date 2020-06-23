c64708b1458b0b0a765ed87030e29d20b0eac0b0
ctdupuis <ctdupuis7@gmail.com>
2020-06-22 23:17:21 -0500

    Done.

---
+++ src/components/Bands.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Band from './Band';
+
+class Bands extends Component {
+  renderBands = () => {
+    return this.props.bands.map(band => <Band key={band.id} band={band} deleteBand={this.props.deleteBand} />)
+  }
+  
+  render() {
+    return (
+      this.renderBands()
+    )
+  }
+}
+
+export default Bands;
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,10 +1,20 @@
+
+
 export default function manageBand(state = {
   bands: []
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
+      const band = {
+      	id: Math.random()*10000,
+      	name: action.name
+      }
 
-      return { ...state, bands: [...state.bands, action.name] }
+      return { ...state, bands: state.bands.concat(band) }
+
+    case 'DELETE_BAND':
+
+      return { ...state, bands: state.bands.filter(band => band.id !== action.id) }
 
     default:
       return state;
